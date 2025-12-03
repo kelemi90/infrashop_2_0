e489f943cfda24852535da4bde00a94fb9da1161
Kimmo Laine <kimmolaine1@kamk.fi>
2025-12-03 17:15:21 +0200

    Test sivun ohjaus

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@viteks/plugin-react';
 
 export default defineConfig({
+    base: '/test/', // <-- tärkeä lisäys, tällä hetkellä frontendin testiversiolle oma polku
     plugins: [react()],
     server: {
         port: 5173,
