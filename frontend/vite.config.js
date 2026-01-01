9da7e75c1d59c29fcfc5a5a549abca02ae70f3c6
Kimmo Laine <kimmolaine1@kamk.fi>
2026-01-02 01:08:45 +0200

    fixed viteks -> vitejs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@viteks/plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: '/test/', // <-- tärkeä lisäys, tällä hetkellä frontendin testiversiolle oma polku
